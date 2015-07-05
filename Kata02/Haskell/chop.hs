f2e5f3b4cababd2c80922783dd567b8c71ca74a8
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-07-05 17:02:27 +1200

    Finish Haskell chop, remove rust exe

--- Kata02/Haskell/chop.hs
+++ Kata02/Haskell/chop.hs
@@ -4,8 +4,8 @@ import Test.HUnit
 chopRecurse :: (Ord a) => Vector a -> a -> Int -> Int -> Int
 chopRecurse xs item low width
   | width <= 0 = -1
-  | xs ! index > item = chopRecurse xs item low ((width - 1) `quot` 2)
-  | xs ! index < item = chopRecurse xs item (index + 1) (width `quot` 2)
+  | xs ! index > item = chopRecurse xs item low (width `quot` 2)
+  | xs ! index < item = chopRecurse xs item (index + 1) ((width - 1) `quot` 2)
   | otherwise = index
   where index = low + width `quot` 2
 
@@ -25,19 +25,51 @@ test5 = TestCase (do let vec = generate  (3) (\i -> i * 2 + 1)
                      assertEqual "chop 3 vec[1, 3, 5]" 1 (chop vec 3))
 test6 = TestCase (do let vec = generate  (3) (\i -> i * 2 + 1)
                      assertEqual "chop 5 vec[1, 3, 5]" 2 (chop vec 5))
-
--- assert_equal(2,  chop(5, [1, 3, 5]))
--- assert_equal(-1, chop(0, [1, 3, 5]))
--- assert_equal(-1, chop(2, [1, 3, 5]))
--- assert_equal(-1, chop(4, [1, 3, 5]))
--- assert_equal(-1, chop(6, [1, 3, 5]))
-
+test7 = TestCase (do let vec = generate  (3) (\i -> i * 2 + 1)
+                     assertEqual "chop 0 vec[1, 3, 5]" (-1) (chop vec 0))
+test8 = TestCase (do let vec = generate  (3) (\i -> i * 2 + 1)
+                     assertEqual "chop 2 vec[1, 3, 5]" (-1) (chop vec 2))
+test9 = TestCase (do let vec = generate  (3) (\i -> i * 2 + 1)
+                     assertEqual "chop 4 vec[1, 3, 5]" (-1) (chop vec 4))
+test10 = TestCase (do let vec = generate  (3) (\i -> i * 2 + 1)
+                      assertEqual "chop 6 vec[1, 3, 5]" (-1) (chop vec 6))
+test11 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 1 vec[1, 3, 5, 7]" 0 (chop vec 1))
+test12 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 3 vec[1, 3, 5, 7]" 1 (chop vec 3))
+test13 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 5 vec[1, 3, 5, 7]" 2 (chop vec 5))
+test14 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 7 vec[1, 3, 5, 7]" 3 (chop vec 7))
+test15 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 0 vec[1, 3, 5, 7]" (-1) (chop vec 0))
+test16 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 2 vec[1, 3, 5, 7]" (-1) (chop vec 2))
+test17 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 4 vec[1, 3, 5, 7]" (-1) (chop vec 4))
+test18 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 6 vec[1, 3, 5, 7]" (-1) (chop vec 6))
+test19 = TestCase (do let vec = generate  (5) (\i -> i * 2 + 1)
+                      assertEqual "chop 8 vec[1, 3, 5, 7]" (-1) (chop vec 8))
 
 tests = TestList [TestLabel "test1" test1,
                   TestLabel "test2" test2,
                   TestLabel "test3" test3,
                   TestLabel "test4" test4,
                   TestLabel "test5" test5,
-                  TestLabel "test6" test6]
+                  TestLabel "test6" test6,
+                  TestLabel "test7" test7,
+                  TestLabel "test8" test8,
+                  TestLabel "test9" test9,
+                  TestLabel "test10" test10,
+                  TestLabel "test11" test11,
+                  TestLabel "test12" test12,
+                  TestLabel "test13" test13,
+                  TestLabel "test14" test14,
+                  TestLabel "test15" test15,
+                  TestLabel "test16" test16,
+                  TestLabel "test17" test17,
+                  TestLabel "test18" test18,
+                  TestLabel "test19" test19]
 
 main = runTestTT tests
