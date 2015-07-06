46555d6a07bd7c8b412ae444fca0dc822c5d56f2
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-07-06 20:57:29 +1200

    Update Java tests to play nice

--- Kata02/Java/src/test/java/ChopTest.java
+++ Kata02/Java/src/test/java/ChopTest.java
@@ -1,9 +1,10 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class ChopTest {
     @Test
     public void emptyChopRecursive() {
-      Chop chop = new ChopRecursive();
+      Chop<Integer> chop = new ChopRecursive<Integer>();
       int index = chop.chop(new Integer[0], 3);
       assertEquals(-1, index);
     }
