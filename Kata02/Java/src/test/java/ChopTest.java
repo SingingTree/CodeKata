7c40abf54d81bbd2884a9bb24a5cc7b97f44d30c
Bryce van Dyk <bryce@vandyk.net.nz>
2015-07-07 14:35:49 +1200

    Kata02: Update and add last tests for recusive chop

--- Kata02/Java/src/test/java/ChopTest.java
+++ Kata02/Java/src/test/java/ChopTest.java
@@ -13,9 +13,35 @@ public class ChopTest {
     public void singleElementChopRecursive() {
         Chop<Integer> chop = new ChopRecursive<Integer>();
         Integer[] array = {1};
-        int index = chop.chop(3, array);
-        assertEquals(-1, index);
-        index = chop.chop(1, array);
-        assertEquals(0, index);
+        assertEquals(-1, chop.chop(3, array));
+        assertEquals(0, chop.chop(1, array));
+    }
+
+    @Test
+    public void threeElementChopRecursive() {
+      Chop<Integer> chop = new ChopRecursive<Integer>();
+      Integer[] array = {1, 3, 5};
+      assertEquals(0, chop.chop(1, array));
+      assertEquals(1, chop.chop(3, array));
+      assertEquals(2, chop.chop(5, array));
+      assertEquals(-1, chop.chop(0, array));
+      assertEquals(-1, chop.chop(2, array));
+      assertEquals(-1, chop.chop(4, array));
+      assertEquals(-1, chop.chop(6, array));
+    }
+
+    @Test
+    public void fiveElementChopRecursive() {
+      Chop<Integer> chop = new ChopRecursive<Integer>();
+      Integer[] array = {1, 3, 5, 7};
+      assertEquals(0, chop.chop(1, array));
+      assertEquals(1, chop.chop(3, array));
+      assertEquals(2, chop.chop(5, array));
+      assertEquals(3, chop.chop(7, array));
+      assertEquals(-1, chop.chop(0, array));
+      assertEquals(-1, chop.chop(2, array));
+      assertEquals(-1, chop.chop(4, array));
+      assertEquals(-1, chop.chop(6, array));
+      assertEquals(-1, chop.chop(8, array));
     }
 }
