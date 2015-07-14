d0e8afb25203248f32894168d7a2820fc801fbbf
Bryce van Dyk <bryce@vandyk.net.nz>
2015-07-14 16:48:13 +1200

    Kata02: Tiny bump to the tests

--- Kata02/Scala/chop/src/test/scala/ChopSpec.scala
+++ Kata02/Scala/chop/src/test/scala/ChopSpec.scala
@@ -7,6 +7,11 @@ class ChopSpec extends FunSpec {
     it("chops empty arrays") {
       assert(-1 == Chop.chop(0, List()))
     }
+
+    it("chops arrays of one element") {
+    	assert(-1 == Chop.chop(3, List(1)))
+    	assert(0 == Chop.chop(1, List(1)))
+    }
   }
 
 }
