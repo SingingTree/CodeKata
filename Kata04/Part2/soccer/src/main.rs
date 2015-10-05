abe09813924dc8fd6a9c093259bfd1b344401624
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-10-05 22:45:45 +1300

    Kata04: Work on part two (looking completed)

--- Kata04/Part2/soccer/src/main.rs
+++ Kata04/Part2/soccer/src/main.rs
@@ -22,45 +22,41 @@ fn read_file(file_name : &str) -> String {
     }
 }
 
-fn parse_table(table : &str) -> Vec<(i32, i32, i32)> {
+fn parse_table(table : &str, team_name_col : usize, col1 : usize, col2 : usize) -> Vec<(String, i32, i32)> {
     let table_lines = table.lines_any();
     let mut parsed_table = Vec::new();
     
     // Skip table header, then ignore any lines that have issues with parsing
-    for line in table_lines.skip(2) {
-        let mut words = line.split_whitespace();
-        print!("Words count {}", words.count());
-        let day = match words.next() {
+    for line in table_lines.skip(1) {
+        let words : Vec<&str> = line.split_whitespace().collect();
+        let team_name = match words.get(team_name_col) {
+            Some(&name) => name,
+            None => continue
+        };
+        let goals_for = match words.get(col1) {
             Some(num) => match num.parse::<i32>() {
                 Ok(num) => num,
                 Err(_) => continue,
             },
             None => continue
         };
-        let max_temp = match words.next() {
-            Some(num) => match num.trim_matches('*').parse::<i32>() {
+        let goals_against = match words.get(col2) {
+            Some(num) => match num.parse::<i32>() {
                 Ok(num) => num,
                 Err(_) => continue,
             },
             None => continue
         };
-        let min_temp = match words.next() {
-            Some(num) => match num.trim_matches('*').parse::<i32>() {
-                Ok(num) => num,
-                Err(_) => continue,
-            },
-            None => continue
-        };
-        parsed_table.push((day, max_temp, min_temp));
+        parsed_table.push((team_name.to_owned(), goals_for, goals_against));
     }
     parsed_table
 }
 
 fn main() {
-    let table_string = read_file("weather.dat");
-    let mut parsed_table = parse_table(&table_string);
+	let table_string = read_file("football.dat");
+    let mut parsed_table = parse_table(&table_string, 1, 6, 8,);
     // Sort vector by spread -- smallest to greatest
-    parsed_table.sort_by(|&(_, a_max, a_min), &(_, b_max, b_min)| (a_max - a_min).cmp(&(b_max - b_min)));
+    parsed_table.sort_by(|&(_, a_for, a_against), &(_, b_for, b_against)| ((a_for - a_against).abs()).cmp(&(b_for - b_against).abs()));
     // Print day with smallest spread
     print!("{}", parsed_table[0].0);
 }
