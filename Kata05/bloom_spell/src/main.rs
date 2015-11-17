df65419278b065e1f36e5c8c1124294412d7c2f3
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-11-17 16:46:42 +1300

    Add word list, crypto crate, start on code

--- Kata05/bloom_spell/src/main.rs
+++ Kata05/bloom_spell/src/main.rs
@@ -1,3 +1,12 @@
+extern crate crypto;
+
+use crypto::digest::Digest;
+
+fn bloom_string<D : Digest>(input : &str, digest : &D) {
+	digest.input_str(input);
+	digest.result_str();
+}
+
 fn main() {
     println!("Hello, world!");
 }
