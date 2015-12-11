96170fa8f809c9f90829ed23b60fbc57799aec6c
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-12-11 22:04:53 +1300

    Kata05: WIP: ruby file reading -- getting going

--- Kata05/ruby/bloom_spell/lib/bloom_spell.rb
+++ Kata05/ruby/bloom_spell/lib/bloom_spell.rb
@@ -1,5 +1,12 @@
 require "bloom_spell/version"
 
-module BloomSpell
-  # Your code goes here...
+class BloomSpeller
+  def load_wordlist(file_name)
+    File.readlines(file_name).each do
+      puts 'foo'
+    end
+  end
 end
+
+bs = BloomSpeller.new
+bs.load_wordlist('wordlist.txt')
