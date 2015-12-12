b6e02e053f06010d6113299e6bb530863e36b7d8
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-12-12 20:42:57 +1300

    Kata05: ruby, take digests of each line in the file

--- Kata05/ruby/bloom_spell/lib/bloom_spell.rb
+++ Kata05/ruby/bloom_spell/lib/bloom_spell.rb
@@ -1,9 +1,17 @@
-require "bloom_spell/version"
+require 'bloom_spell/version'
+require 'bitarray'
+require 'openssl'
 
 class BloomSpeller
+  def initialize
+    @bit_array_size = 100_000
+    @bit_array = BitArray.new(@bit_array_size)
+    @digest = OpenSSL::Digest::SHA256.new
+  end
+
   def load_wordlist(file_name)
-    File.readlines(file_name).each do
-      puts 'foo'
+    File.readlines(file_name).each do |line|
+      puts @digest.hexdigest(line)
     end
   end
 end
