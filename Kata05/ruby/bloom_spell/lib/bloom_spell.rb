b9d5bd85811648e38bd3ad0ca7ef9ef4909510e3
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-12-15 13:33:43 +1300

    Kata05: Finish up working draft of ruby version

--- Kata05/ruby/bloom_spell/lib/bloom_spell.rb
+++ Kata05/ruby/bloom_spell/lib/bloom_spell.rb
@@ -4,17 +4,42 @@ require 'openssl'
 
 class BloomSpeller
   def initialize
-    @bit_array_size = 100_000
+    @bit_array_size = 1_000_000
     @bit_array = BitArray.new(@bit_array_size)
     @digest = OpenSSL::Digest::SHA256.new
+    @num_chunks = 4
+  end
+
+  def bloom_word(word)
+    hash = @digest.hexdigest(word)
+    chunk_size = hash.length / @num_chunks
+    for i in 0..@num_chunks - 1
+      sub_hash = hash[i..i + chunk_size - 1]
+      @bit_array[sub_hash.to_i(16) % @bit_array_size] = 1
+    end
   end
 
   def load_wordlist(file_name)
     File.readlines(file_name).each do |line|
-      puts @digest.hexdigest(line)
+      bloom_word(line)
     end
+  end
+
+  def check_word(word)
+    hash = @digest.hexdigest(word)
+    chunk_size = hash.length / @num_chunks
+    for i in 0..@num_chunks - 1
+      sub_hash = hash[i..i + chunk_size - 1]
+      if @bit_array[sub_hash.to_i(16) % @bit_array_size] == 0
+        return false
+      end
+    end
+    true
   end
 end
 
 bs = BloomSpeller.new
 bs.load_wordlist('wordlist.txt')
+
+puts "cats: #{bs.check_word('cats')}"
+puts "catz: #{bs.check_word('catz')}"
