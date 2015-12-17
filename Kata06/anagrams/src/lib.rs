3eed61534e9738b5810978692368aef48cd9f8b3
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-12-17 16:25:52 +1300

    Kata06: add function to get char freqs from string

--- Kata06/anagrams/src/lib.rs
+++ Kata06/anagrams/src/lib.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+fn get_char_freqs(string : &str) -> HashMap<char, usize> {
+    let mut freqs = HashMap::new();
+    for c in string.chars() {
+        let counter = freqs.entry(c).or_insert(0);
+        *counter += 1;
+    }
+    freqs
+}
+
 #[test]
-fn it_works() {
+fn get_freqs_works() {
+    let string = "waffles";
+    let freqs = get_char_freqs(string);
+    assert_eq!(freqs[&'w'], 1);
+    assert_eq!(freqs[&'a'], 1);
+    assert_eq!(freqs[&'f'], 2);
+    assert_eq!(freqs[&'l'], 1);
+    assert_eq!(freqs[&'e'], 1);
+    assert_eq!(freqs[&'s'], 1);
 }
